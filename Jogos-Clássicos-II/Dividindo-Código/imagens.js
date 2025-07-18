b5361a8b203dfde4a537c608c4fcca4dadfc74ad
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-17 23:36:26 -0300

    Create imagens.js

---
+++ Jogos-Clássicos-II/Dividindo-Código/imagens.js
@@ -0,0 +1,12 @@
+imagens.js
+
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+
+function preload(){
+  imagemDaEstrada = loadImage("Imagens/estrada.png");
+  imagemDaEstrada = loadImage("Imagens/ator-1.png");
+  imagemDaEstrada = loadImage("Imagens/carro-1.png");
+  
+}
