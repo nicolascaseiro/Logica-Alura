dd112d189e166fad41ad107fdd26d29c559df5c6
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-17 23:58:58 -0300

    Update imagens.js

--- Jogos-Clássicos-II/free_way/imagens.js
+++ Jogos-Clássicos-II/free_way/imagens.js
@@ -3,13 +3,12 @@ let imagemAtor;
 let imagemCarros;
 
 function preload(){
-  imagemEstrada = loadImage('imagens/estrada.png') //carregar imagem
-  imagemAtor = loadImage('imagens/ator-1.png')
-  //array imagens carros
+  imagemEstrada = loadImage('imagens/estrada.png');
+  imagemAtor = loadImage('imagens/ator-1.png');
   imagemCarros = [loadImage('imagens/carro-1.png'),
                   loadImage('imagens/carro-2.png'),
                   loadImage('imagens/carro-3.png'),
                   loadImage('imagens/carro-1.png'),
                   loadImage('imagens/carro-2.png'),
-                  loadImage('imagens/carro-3.png')]
+                  loadImage('imagens/carro-3.png')];
 }
