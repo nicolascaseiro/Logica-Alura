c8eb5b60a1499b2a7a1fd9330438346ea3d4f147
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-07-17 23:39:03 -0300

    Create carros.js

---
+++ Jogos-Clássicos-II/Dividindo-Código/carros.js
@@ -0,0 +1,13 @@
+//código do carro
+
+let xCarros = 600
+let yCarros = 40
+
+function mostraCarro(){
+  image(imagemCarro, xCarro, yCarro, 50, 40);
+  }
+}
+function movimentaCarro(){
+  xCarro -=2
+  }
+}
